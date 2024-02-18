b3ed5882284dca698d03cf2e2b427452f712672e
awsdevopsonline <punnam.elite@gmail.com>
2024-02-18 23:20:56 +0530

    TEST

--- vars/nodejsVM.groovy
+++ vars/nodejsVM.groovy
@@ -28,98 +28,95 @@ pipeline {
         // password(name: 'PASSWORD', defaultValue: 'SECRET', description: 'Enter a password')
     }
     // build
-    stages {
-        stage('Get the version') {
-            steps {
-                script {
-                    def packageJson = readJSON file: 'package.json'
-                    packageVersion = packageJson.version
-                    echo "application version: $packageVersion"
-                }
-            }
-        }
-        stage('Install dependencies') {
-            steps {
-                sh """
-                    npm install
-                """
-            }
-        }
-        stage('Unit tests') {
-            steps {
-                sh """
-                    echo "unit tests will run here"
-                """
-            }
-        }
-        stage('Sonar Scan') {
-             steps {
-                sh """
-                     echo "sonar scan will run here"
-                """
-             }
-        }
-
-        
-        stage('Build') {
-            steps {
-                sh """
-                    ls -la
-                    zip -q -r ${configMap.component}.zip ./* -x ".git" -x "*.zip"
-                    ls -ltr
-                """
-            }
-        }
-        stage('Publish Artifact') {
-            steps {
-                 nexusArtifactUploader(
-                    nexusVersion: 'nexus3',
-                    protocol: 'http',
-                    nexusUrl: pipelineGlobals.nexusURL,
-                    groupId: 'com.roboshop',
-                    version: "${packageVersion}",
-                    repository: "${configMap.component}",
-                    credentialsId: 'nexus-auth',
-                    artifacts: [
-                        [artifactId: "${configMap.component}",
-                        classifier: '',
-                        file: "${configMap.component}.zip",
-                        type: 'zip']
-                    ]
-                )
-            }
-        }
-        stage('Deploy') {
-            
-            when {
-                expression{
-                    params.Deploy == 'true'
-                }
-            }
-            steps {
-                script {
-                        def params = [
-                            string(name: 'version', value: "$packageVersion"),
-                            string(name: 'environment', value: "dev")
-                        ]
-                        build job: "${configMap.component}-deploy", wait: true, parameters: params
+        stages {
+            stage('Get the version') {
+                steps {
+                    script {
+                        def packageJson = readJSON file: 'package.json'
+                        packageVersion = packageJson.version
+                        echo "application version: $packageVersion"
                     }
+                }
             }
-        }              
-}
-         
-    // post build
-    post { 
-        always { 
-            echo 'I will always say Hello again!'
-            deleteDir()
+            stage('Install dependencies') {
+                steps {
+                    sh """
+                        npm install
+                    """
+                }
+            }
+            stage('Unit tests') {
+                steps {
+                    sh """
+                        echo "unit tests will run here"
+                    """
+                }
+            }
+            stage('Sonar Scan'){
+                steps{
+                    sh """
+                        echo "usually command here is sonar-scanner"
+                        echo "sonar scan will run here"
+                    """
+                }
+            }
+            stage('Build') {
+                steps {
+                    sh """
+                        ls -la
+                        zip -q -r ${configMap.component}.zip ./* -x ".git" -x "*.zip"
+                        ls -ltr
+                    """
+                }
+            }
+            stage('Publish Artifact') {
+                steps {
+                    nexusArtifactUploader(
+                        nexusVersion: 'nexus3',
+                        protocol: 'http',
+                        nexusUrl: pipelineGlobals.nexusURL(),
+                        groupId: 'com.roboshop',
+                        version: "${packageVersion}",
+                        repository: "${configMap.component}",
+                        credentialsId: 'nexus-auth',
+                        artifacts: [
+                            [artifactId: "${configMap.component}",
+                            classifier: '',
+                            file: "${configMap.component}.zip",
+                            type: 'zip']
+                        ]
+                    )
+                }
+            }
+            stage('Deploy') {
+                when {
+                    expression{
+                        params.Deploy
+                    }
+                }
+                steps {
+                    script {
+                            def params = [
+                                string(name: 'version', value: "$packageVersion"),
+                                string(name: 'environment', value: "dev")
+                            ]
+                            build job: "../${configMap.component}-deploy", wait: true, parameters: params
+                        }
+                }
+            }
         }
-        failure { 
-            echo 'this runs when pipeline is failed, used generally to send some alerts'
-        }
-        success{
-            echo 'I will say Hello when pipeline is success'
+        // post build
+        post { 
+            always { 
+                echo 'I will always say Hello again!'
+                deleteDir()
+            }
+            failure { 
+                echo 'this runs when pipeline is failed, used generally to send some alerts'
+            }
+            success{
+                echo 'I will say Hello when pipeline is success'
+            }
         }
     }
-}
 }
